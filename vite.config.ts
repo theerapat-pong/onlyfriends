761cd942bc26ba66dbbde1f2827732727dd83c3c
Aung Kyaw Min <theerapat.mdm@gmail.com>
2025-07-10 07:00:14 +0700

    Fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import process from 'process';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
